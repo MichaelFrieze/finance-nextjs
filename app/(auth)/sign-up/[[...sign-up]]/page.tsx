c901e6d3502c16efdabfb6c5389eb773320dfea2
Michael Frieze <michaelfrieze@icloud.com>
2024-05-19 16:34:41 -0400

    feat: finish setting up auth

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,29 @@
-const SignUpPage = () => {
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+export default function Page() {
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        {/* <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A47]">Welcome!</h1>
+          <p className="text-base text-[#7E8CA0]">
+            Create an account to get to your dashboard.
+          </p>
+        </div> */}
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
+        <Image src={"/logo.svg"} height={100} width={100} alt="Logo" />
+      </div>
     </div>
   );
-};
-
-export default SignUpPage;
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <p>This is an authenticated route</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
