2f85200407f1ad81524ab4f31b12f59cd22f8341
Michael Frieze <michaelfrieze@icloud.com>
2024-05-23 18:45:13 -0400

    feat: add a basic dashboard layout and header

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,8 +3,8 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <p>This is an authenticated route</p>
-      <UserButton afterSignOutUrl="/" />
+      <p>Header Component</p>
+      {/* <UserButton afterSignOutUrl="/" /> */}
     </div>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 pb-36 lg:px-14">
+      <div className="mx-auto max-w-screen-2xl">Header</div>
+    </header>
+  );
+};
