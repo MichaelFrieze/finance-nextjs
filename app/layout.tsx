35b4079c9f725894fa64cac00d1c42f41cb00dbf
Michael Frieze <michaelfrieze@icloud.com>
2024-06-12 19:14:59 -0400

    feat: add accounts POST API

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 import { QueryProviders } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +23,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <QueryProviders>{children}</QueryProviders>
+          <QueryProviders>
+            <SheetProvider />
+            <Toaster />
+            {children}
+          </QueryProviders>
         </body>
       </html>
     </ClerkProvider>
