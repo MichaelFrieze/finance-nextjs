30ce2136c9a0b85d891d5e3ac807227d0b27be4a
Michael Frieze <michaelfrieze@icloud.com>
2024-06-25 13:16:25 -0400

    feat: add dashboard chart

---
+++ components/charts.tsx
@@ -0,0 +1,80 @@
+import { AreaChart, BarChart, FileSearch, LineChart } from "lucide-react";
+import { useState } from "react";
+
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+  SelectItem,
+} from "@/components/ui/select";
+import { Card, CardTitle, CardHeader, CardContent } from "@/components/ui/card";
+import { AreaVariant } from "./area-variant";
+import { BarVariant } from "./bar-variant";
+import { LineVariant } from "./line-variant";
+
+type Props = {
+  data?: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+};
+
+export const Charts = ({ data = [] }: Props) => {
+  const [chartType, setChartType] = useState("area");
+
+  const onTypeChange = (type: string) => {
+    // TODO: Add paywall
+    setChartType(type);
+  };
+
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex justify-between space-y-2 lg:flex-row lg:items-center lg:space-y-0">
+        <CardTitle className="line-clamp-1 text-xl">Transactions</CardTitle>
+        <Select defaultValue={chartType} onValueChange={onTypeChange}>
+          <SelectTrigger className="h-9 rounded-md px-3 lg:w-auto">
+            <SelectValue placeholder="Chart Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="area">
+              <div className="flex items-center">
+                <AreaChart className="mr-2 size-4 shrink-0" />
+                <p className="line-clamp-1">Area Chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="line">
+              <div className="flex items-center">
+                <LineChart className="mr-2 size-4 shrink-0" />
+                <p className="line-clamp-1">Line Chart</p>
+              </div>
+            </SelectItem>
+            <SelectItem value="bar">
+              <div className="flex items-center">
+                <BarChart className="mr-2 size-4 shrink-0" />
+                <p className="line-clamp-1">Bar Chart</p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent>
+        {data.length === 0 ? (
+          <div className="flex h-[350px] w-full flex-col items-center justify-center gap-y-4">
+            <FileSearch className="size-6 text-muted-foreground" />
+            <p className="text-sm text-muted-foreground">
+              No data for this period
+            </p>
+          </div>
+        ) : (
+          <>
+            {chartType === "line" && <LineVariant data={data} />}
+            {chartType === "area" && <AreaVariant data={data} />}
+            {chartType === "bar" && <BarVariant data={data} />}
+          </>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
