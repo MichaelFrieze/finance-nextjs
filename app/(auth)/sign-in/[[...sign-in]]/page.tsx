c163080ca113c215645f6f168927a7abb0f25e0f
Michael Frieze <michaelfrieze@icloud.com>
2024-07-02 11:44:15 -0400

    change: remove the header text above the clerk auth components on the sign-in and sign-up pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,19 +1,17 @@
+import Image from "next/image";
 import { Loader2 } from "lucide-react";
 import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
       <div className="h-full flex-col items-center justify-center px-4 lg:flex">
-        <div className="space-y-4 pt-16 text-center">
-          <h1 className="text-3xl font-bold text-[#2E2A47] text-muted-foreground">
-            Welcome Back!
-          </h1>
+        {/* <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A47]">Welcome Back!</h1>
           <p className="text-base text-[#7E8CA0]">
-            Log in or Create account to get back to your dashboard!
+            Log in to get to your dashboard.
           </p>
-        </div>
+        </div> */}
         <div className="mt-8 flex items-center justify-center">
           <ClerkLoaded>
             <SignIn path="/sign-in" />
@@ -24,7 +22,7 @@ export default function Page() {
         </div>
       </div>
       <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
-        <Image src="/logo.svg" alt="logo" height={100} width={100} />
+        <Image src={"/logo.svg"} height={100} width={100} alt="Logo" />
       </div>
     </div>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,19 +1,17 @@
+import Image from "next/image";
 import { Loader2 } from "lucide-react";
 import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
       <div className="h-full flex-col items-center justify-center px-4 lg:flex">
-        <div className="space-y-4 pt-16 text-center">
-          <h1 className="text-3xl font-bold text-[#2E2A47] text-muted-foreground">
-            Welcome Back!
-          </h1>
+        {/* <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A47]">Welcome!</h1>
           <p className="text-base text-[#7E8CA0]">
-            Log in or Create account to get back to your dashboard!
+            Create an account to get to your dashboard.
           </p>
-        </div>
+        </div> */}
         <div className="mt-8 flex items-center justify-center">
           <ClerkLoaded>
             <SignUp path="/sign-up" />
@@ -24,7 +22,7 @@ export default function Page() {
         </div>
       </div>
       <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
-        <Image src="/logo.svg" alt="logo" height={100} width={100} />
+        <Image src={"/logo.svg"} height={100} width={100} alt="Logo" />
       </div>
     </div>
   );
